2838856b788b0cceb9032e7ad237395cf4fd8a3b
Alex-Berezov <aleber197@gmail.com>
2023-06-19 13:24:02 +0400

    Start progect

---
+++ src/services/alphaVantageService.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosResponse } from 'axios'
+
+const alphaVantageAPI = axios.create({
+  baseURL: 'https://www.alphavantage.co/query',
+  params: {
+    apikey: process.env.ALPHA_VANTAGE_API_KEY,
+  },
+})
+
+export const fetchTimeSeries = async (
+  symbol: string
+): Promise<AxiosResponse<any>> => {
+  try {
+    const response = await alphaVantageAPI.get('/', {
+      params: {
+        function: 'TIME_SERIES_INTRADAY',
+        symbol: symbol,
+        interval: '5min',
+        outputsize: 'full',
+      },
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error fetching data from Alpha Vantage', error)
+    throw error
+  }
+}
