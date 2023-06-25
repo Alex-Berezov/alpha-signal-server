085ccb6a0f53376900e5498a0feec725b005e4ba
Alex-Berezov <aleber197@gmail.com>
2023-06-25 13:49:43 +0400

    Fixed request to AlphaVantage

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { fetchTimeSeries } from 'services/alphaVantageService'
+import { fetchTimeSeries } from './services/alphaVantageService'
 
 dotenv.config()
 
@@ -18,8 +18,23 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/timeseries/:symbol', async (req, res) => {
   try {
     const symbol = req.params.symbol
-    const response = await fetchTimeSeries(symbol)
-    res.json(response)
+    const apiKey = process.env.ALPHA_VANTAGE_API_KEY!
+    const response = await fetchTimeSeries(symbol, apiKey)
+
+    // Обработка полученного массива котировок
+    const timeSeries = response['Time Series (5min)']
+    const quotes = Object.keys(timeSeries).map((timestamp) => {
+      return {
+        timestamp,
+        open: timeSeries[timestamp]['1. open'],
+        high: timeSeries[timestamp]['2. high'],
+        low: timeSeries[timestamp]['3. low'],
+        close: timeSeries[timestamp]['4. close'],
+        volume: timeSeries[timestamp]['5. volume'],
+      }
+    })
+
+    res.json(quotes)
   } catch (error) {
     console.error('Error fetching time series data', error)
     res.status(500).json({ error: 'Internal Server Error' })
--- src/services/alphaVantageService.ts
+++ src/services/alphaVantageService.ts
@@ -1,15 +1,26 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
+
+interface AlphaVantageResponse {
+  'Time Series (5min)': Record<
+    string,
+    {
+      '1. open': string
+      '2. high': string
+      '3. low': string
+      '4. close': string
+      '5. volume': string
+    }
+  >
+}
 
 const alphaVantageAPI = axios.create({
   baseURL: 'https://www.alphavantage.co/query',
-  params: {
-    apikey: process.env.ALPHA_VANTAGE_API_KEY,
-  },
 })
 
 export const fetchTimeSeries = async (
-  symbol: string
-): Promise<AxiosResponse<any>> => {
+  symbol: string,
+  apiKey: string
+): Promise<AlphaVantageResponse> => {
   try {
     const response = await alphaVantageAPI.get('/', {
       params: {
@@ -17,9 +28,10 @@ export const fetchTimeSeries = async (
         symbol: symbol,
         interval: '5min',
         outputsize: 'full',
+        extended_hours: false,
+        apikey: apiKey,
       },
     })
-    console.log(response.data)
     return response.data
   } catch (error) {
     console.error('Error fetching data from Alpha Vantage', error)
